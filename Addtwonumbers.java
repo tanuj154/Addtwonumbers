8cbb367809fac0ae62cbb524c2ef5186a27a2677
tanuj154 <91303364+tanuj154@users.noreply.github.com>
2021-12-02 11:03:08 +0530

    Add files via upload

---
+++ Addtwonumbers.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class Addtwonumbers {
+      public static void main(String[] args) 
+      {
+        int num1 = 5, num2 = 15, sum;
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+      }
+    
+}
